fbfdf6a3b51f771c303ba1f2f940d1cf0ceb7552
changfei <tianwening@163.com>
2018-06-03 22:52:02 +0800

    vue.js上手学习

---
+++ 01/src/js/app.vue
@@ -0,0 +1,30 @@
+<!-- template、script和style -->
+<template>
+    <!-- vue2.x以后的版本只能指定一个根目录 -->
+    <div>
+        <header-vue></header-vue>
+        <button v-on:click="listen">爸爸焦急的听电话</button>
+    </div>
+</template>
+<script>
+
+import connector from './connector.js';
+
+export default {
+
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        listen(){
+            connector.$on('phone',function(msg){
+                console.log(msg);
+            });
+        }
+    }
+}
+</script>
+<style>
+</style>
---
+++ 01/src/js/components/hearder.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        我是头部
+        <button v-on:click="callDaddy">打电话给爸爸</button>
+    </div>
+</template>
+<script>
+import connector from '../connector.js';
+export default {
+	data(){
+		return {
+
+		}
+	},
+	methods: {
+		callDaddy(){
+			connector.$emit('phone','62分钟来');
+		}
+	}
+}
+</script>
+<style scoped>
+	div {
+	    height: 100px;
+	    background-color: pink;
+	}
+</style>
